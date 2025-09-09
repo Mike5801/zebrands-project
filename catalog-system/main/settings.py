b3c274fa0eb52387f1c87765ac146f0d4536db0e
Mike5801 <miguelw.tangf@gmail.com>
2025-09-08 22:56:50 -0600

    fix environment variable declaration in github workflow

--- catalog-system/main/settings.py
+++ catalog-system/main/settings.py
@@ -121,11 +121,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["NAME"],
-        'USER': os.environ["USER"],
-        'PASSWORD': os.environ["PASSWORD"],
-        'HOST': os.environ["HOST"],
-        'PORT': os.environ["PORT"],
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
     }
 }
 
