2ccb151e6fe24bcb69eef82bf7140fa915ff6e86
Mike5801 <miguelw.tangf@gmail.com>
2025-09-08 19:55:55 -0600

    add CRUD for products

--- catalog-system/api/urls.py
+++ catalog-system/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import get_products
+from .views import get_products, create_product, get_single_product, update_product, delete_product
 
 urlpatterns = [
-    path("products/", get_products, name="get_products")
+    path("products/", get_products, name="get_products"),
+    path("products/create/", create_product, name="create_product"),
+    path("products/<str:id>", get_single_product, name="get_single_product"),
+    path("products/update/<str:id>", update_product, name="update_product"),
+    path("products/delete/<str:id>", delete_product, name="delete_product")
 ]
--- catalog-system/api/views.py
+++ catalog-system/api/views.py
@@ -6,4 +6,44 @@ from .serializer import ProductSerializer
 
 @api_view(["GET"])
 def get_products(request):
-    return Response(ProductSerializer({""}).data)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+def get_single_product(request, id):
+    try:
+        product = Product.objects.get(sku=id)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+    
+@api_view(["PUT"])
+def update_product(request, id):
+    try:
+        product = Product.objects.get(sku=id)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["DELETE"])
+def delete_product(request, id):
+    try:
+        product = Product.objects.get(sku=id)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
